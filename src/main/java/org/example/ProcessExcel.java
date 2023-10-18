57e5b4259ecd51e158d57a20cecae2566ecffb1f
Zhenyettta <zhecky2004@gmail.com>
2023-10-18 15:13:34 +0300

    Add JavaDoc comments for ProcessExcel class

--- src/main/java/org/example/ProcessExcel.java
+++ src/main/java/org/example/ProcessExcel.java
@@ -8,7 +8,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 /**
  * The {@code ProcessExcel} class is responsible for processing Excel workbooks and extracting relevant data.
@@ -23,9 +22,14 @@ public class ProcessExcel {
 
     private String tempTime = null;
     private String tempWeekday = null;
-    private String specialityRegex = "«(.*?)»";
 
 
+    /**
+     * Extracts a string containing the keyword "DEAN" from the given workbook.
+     *
+     * @param workbook the workbook from which to extract the dean string
+     * @return the extracted dean string with underscores removed
+     */
     String extractDean(Workbook workbook) {
         Sheet sheet = workbook.getSheetAt(0);
         StringBuilder deanString = new StringBuilder();
@@ -37,6 +41,13 @@ public class ProcessExcel {
         return deanString.toString().replace("_", "");
     }
 
+    /**
+     * Processes a list of file paths and returns a list of Department objects.
+     *
+     * @param paths the list of file paths to process
+     * @return a list of Department objects
+     * @throws RuntimeException if an error occurs while processing the workbooks
+     */
     List<Department> processData(List<String> paths) {
         return paths.stream().map(path -> {
             File file = new File(path);
@@ -52,6 +63,12 @@ public class ProcessExcel {
         }).toList();
     }
 
+    /**
+     * Processes an IPZ workbook and returns a Department object.
+     *
+     * @param workbook the IPZ workbook to process
+     * @return a Department object
+     */
     private Department processIpzWorkbook(Workbook workbook) {
         String departmentName = extractDepartmentName(workbook);
         String specialityName = extractSpecialityName(workbook);
@@ -60,6 +77,12 @@ public class ProcessExcel {
         return createDepartment(departmentName, specialityName, disciplines);
     }
 
+    /**
+     * Processes the given workbook to create a department for the FEN (Faculty of Engineering) department.
+     *
+     * @param workbook The workbook to process.
+     * @return The created department.
+     */
     private Department processFenWorkbook(Workbook workbook) {
         String departmentName = extractDepartmentName(workbook);
         List<String> specialityNames = extractFenSpecialityName(workbook);
@@ -68,6 +91,12 @@ public class ProcessExcel {
         return createFenDepartment(departmentName, specialityNames, disciplines);
     }
 
+    /**
+     * Extracts the department name from the given workbook.
+     *
+     * @param workbook The workbook to extract the department name from.
+     * @return The extracted department name.
+     */
     private String extractDepartmentName(Workbook workbook) {
         Sheet sheet = workbook.getSheetAt(0);
         StringBuilder departmentString = new StringBuilder();
@@ -79,6 +108,12 @@ public class ProcessExcel {
         return departmentString.toString();
     }
 
+    /**
+     * Extracts the speciality name from the given workbook's sheet at index 0.
+     *
+     * @param workbook the workbook object containing the sheet with speciality name
+     * @return the extracted speciality name as a String
+     */
     private String extractSpecialityName(Workbook workbook) {
         Sheet sheet = workbook.getSheetAt(0);
         StringBuilder specialityString = new StringBuilder();
@@ -90,8 +125,15 @@ public class ProcessExcel {
         return specialityString.toString().replace("\"", "");
     }
 
+    /**
+     * Extracts the FEN speciality names from the given workbook's sheet at index 0.
+     *
+     * @param workbook the workbook object containing the sheet with FEN speciality names
+     * @return a list of extracted FEN speciality names as strings
+     */
     private List<String> extractFenSpecialityName(Workbook workbook) {
         Sheet sheet = workbook.getSheetAt(0);
+        String specialityRegex = "«(.*?)»";
         Pattern pattern = Pattern.compile(specialityRegex);
 
 
@@ -107,6 +149,12 @@ public class ProcessExcel {
         return specialities;
     }
 
+    /**
+     * Extracts a list of disciplines from the given sheet, starting from the specified first row.
+     *
+     * @param sheet    The sheet from which to extract the disciplines.
+     * @param firstRow The index of
+     */
     private List<Discipline> extractDisciplines(Sheet sheet, int firstRow) {
         List<Discipline> disciplines = new ArrayList<>();
         int lastRow = sheet.getLastRowNum();
@@ -120,6 +168,14 @@ public class ProcessExcel {
         return disciplines;
     }
 
+    /**
+     * Creates a new department with the given department name, speciality name, and list of disciplines.
+     *
+     * @param departmentName the name of the department
+     * @param specialityName the name of the speciality
+     * @param disciplines    the list of disciplines
+     * @return the created department
+     */
     private Department createDepartment(String departmentName, String specialityName, List<Discipline> disciplines) {
         Department department = new Department(departmentName);
         Speciality speciality = new Speciality(specialityName);
@@ -128,6 +184,14 @@ public class ProcessExcel {
         return department;
     }
 
+    /**
+     * Creates a Department object for the FEN department.
+     *
+     * @param departmentName  the name of the department
+     * @param specialityNames the list of speciality names
+     * @param disciplines     the list of disciplines
+     * @return the Department object for the FEN department
+     */
     private Department createFenDepartment(String departmentName, List<String> specialityNames, List<Discipline> disciplines) {
         Department department = new Department(departmentName);
         List<Speciality> specialities = specialityNames.stream().map(Speciality::new).toList();
@@ -137,52 +201,94 @@ public class ProcessExcel {
         return department;
     }
 
+    /**
+     * Inserts disciplines into appropriate specialities in the given department.
+     *
+     * @param department  the department to insert specialities into
+     * @param disciplines the list of disciplines to insert
+     * @return the department with inserted specialities
+     */
     private Department insertSpecialities(Department department, List<Discipline> disciplines) {
-        disciplines.forEach(i -> {
-            String regex = "\\(([^)]+)\\)";
-            String regex2 = "^[^()]*$";
-            Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(i.getName());
-            Pattern pattern2 = Pattern.compile(regex2);
-            Matcher matcher2 = pattern2.matcher(i.getName());
-            if(matcher2.find()){
-                department.getSpecialities().forEach(speciality -> speciality.getDisciplines().add(filterFenDiscipline(i)));
+        final String parenthesesRegex = "\\(([^)]+)\\)";
+        final String noParenthesesRegex = "^[^()]*$";
+        final Pattern parenthesesPattern = Pattern.compile(parenthesesRegex);
+        final Pattern noParenthesesPattern = Pattern.compile(noParenthesesRegex);
+
+        for (Discipline discipline : disciplines) {
+            Matcher matcher = parenthesesPattern.matcher(discipline.getName());
+            Matcher matcher2 = noParenthesesPattern.matcher(discipline.getName());
+            if (matcher2.find()) {
+                addDisciplineToAllSpecialities(department, discipline);
             }
+
             while (matcher.find()) {
-                String match = matcher.group(1).trim();
-                String[] splitSpecialities;
-
-                final boolean[] patternFound = {false};
-
-                if (match.contains("+")) {
-                    splitSpecialities = match.split("\\+");
-                    patternFound[0] = true;
-                } else if (match.contains(".")) {
-                    splitSpecialities = match.split("\\.");
-                    patternFound[0] = true;
-                } else if (match.contains(",")) {
-                    splitSpecialities = match.split(",");
-                    patternFound[0] = true;
-                } else {
-                    splitSpecialities = new String[]{match};
-                    patternFound[0] = true;
-                }
-
-                department.getSpecialities().forEach(speciality -> {
-                    for (String name : splitSpecialities) {
-                        if (speciality.getName().toLowerCase().contains(name.toLowerCase())) {
-                            speciality.getDisciplines().add(filterFenDiscipline(i));
-                            patternFound[0] = true;
-                        }
-                    }
-                });
+                String[] splitSpecialities = splitMatches(matcher.group(1).trim());
+                addDisciplineToAppropriateSpecialities(department, discipline, splitSpecialities);
             }
+        }
 
-
-        });
         return department;
     }
 
+    /**
+     * Adds a discipline to all specialities in the given department
+     */
+    private void addDisciplineToAllSpecialities(Department department, Discipline discipline) {
+        department.getSpecialities().forEach(speciality -> speciality.getDisciplines().add(filterFenDiscipline(discipline)));
+    }
+
+    /**
+     * Splits the given match string into an array of substrings based on certain delimiters.
+     * If the match contains the delimiter '+', it will split the match using '+'.
+     * If the match contains the delimiter '.', it will split the match using '.'.
+     * If the match contains the delimiter ',', it will split the match using ','.
+     * If the match does not contain any of the delimiters, it will create a new array with the match as the only element.
+     *
+     * @param match the string to be splitted
+     * @return an array of
+     */
+    private String[] splitMatches(String match) {
+        String[] splitSpecialities;
+
+        if (match.contains("+")) {
+            splitSpecialities = match.split("\\+");
+        } else if (match.contains(".")) {
+            splitSpecialities = match.split("\\.");
+        } else if (match.contains(",")) {
+            splitSpecialities = match.split(",");
+        } else {
+            splitSpecialities = new String[]{match};
+        }
+
+        return splitSpecialities;
+    }
+
+    /**
+     * Adds a discipline to appropriate specialities in a department.
+     *
+     * @param department        the department to add the discipline to
+     * @param discipline        the discipline to add
+     * @param splitSpecialities an array of speciality names to match and add the discipline to
+     */
+    private void addDisciplineToAppropriateSpecialities(Department department, Discipline discipline, String[] splitSpecialities) {
+        department.getSpecialities().forEach(speciality -> {
+            for (String name : splitSpecialities) {
+                if (speciality.getName().toLowerCase().contains(name.toLowerCase())) {
+                    speciality.getDisciplines().add(filterFenDiscipline(discipline));
+                }
+            }
+        });
+    }
+
+    /**
+     * Filters the given discipline by modifying its group and auditorium properties.
+     * If the discipline's group contains the substring "лекція" (case-insensitive), it will be modified to "Лекція".
+     * If the discipline's auditorium is "Д" (case-insensitive), it will be modified to "Дистанційно".
+     * The group will be modified to contain only digits.
+     *
+     * @param discipline The discipline to filter.
+     * @return The filtered discipline.
+     */
     public Discipline filterFenDiscipline(Discipline discipline) {
         if (discipline.getGroup().toLowerCase().contains("лекція")) discipline.setGroup("Лекція");
         if (discipline.getAuditorium().equalsIgnoreCase("Д")) discipline.setAuditorium("Дистанційно");
@@ -198,6 +304,12 @@ public class ProcessExcel {
         return discipline;
     }
 
+    /**
+     * Processes a single row to extract information and create a Discipline object.
+     *
+     * @param row the row to process
+     * @return a Discipline object created from the row information
+     */
     public Discipline processRow(Row row) {
         Iterator<Cell> cellIterator = row.cellIterator();
         String day = processDay(cellIterator);
@@ -214,6 +326,12 @@ public class ProcessExcel {
         return new Discipline(discipline, day, time, group, weeks, auditorium);
     }
 
+    /**
+     * Processes the day value from the given cellIterator.
+     *
+     * @param cellIterator the iterator containing the cells to process
+     * @return the processed day value
+     */
     private String processDay(Iterator<Cell> cellIterator) {
         String day = tempWeekday;
         if (cellIterator.hasNext()) {
@@ -227,6 +345,12 @@ public class ProcessExcel {
         return day;
     }
 
+    /**
+     * Processes the time value from the given iterator.
+     *
+     * @param cellIterator the iterator of cells containing the time value
+     * @return the processed time value
+     */
     private String processTime(Iterator<Cell> cellIterator) {
         String time = tempTime;
         if (cellIterator.hasNext()) {
@@ -240,6 +364,11 @@ public class ProcessExcel {
         return time;
     }
 
+    /**
+     * Processes the value of a cell in the given iterator.
+     *
+     * @param cellIterator The iterator containing the cells.
+     */
     private String processCellValue(Iterator<Cell> cellIterator) {
         if (cellIterator.hasNext()) {
             return getCellValue(cellIterator.next());
@@ -247,6 +376,13 @@ public class ProcessExcel {
         return null;
     }
 
+    /**
+     * Returns the value of a Cell as a String.
+     *
+     * @param cell The Cell to get the value from.
+     * @return The value of the Cell as a String. If the Cell is of type STRING, the String value is returned.
+     * If the Cell is of type NUMERIC, the
+     */
     String getCellValue(Cell cell) {
         return switch (cell.getCellType()) {
             case STRING -> cell.getStringCellValue();
@@ -257,8 +393,6 @@ public class ProcessExcel {
 
     /**
      * Helper method to retrieve the trimmed string value of a cell
-     * @param cell The cell for which to retrieve the trimmed string
-     * @return The trimmed string value of the cell
      */
     private String getTrimmedCellValue(Cell cell) {
         return cell.getStringCellValue().trim();
@@ -267,9 +401,9 @@ public class ProcessExcel {
     /**
      * This method finds the first occurrence of a search string in a Workbook,
      * searching row by row, cell by cell.
-     * @param workbook The Workbook object to search
+     *
+     * @param workbook     The Workbook object to search
      * @param searchString The string to find in the Workbook
-     * @return Returns the row number where the searchString was first found, or -1 if not found
      */
     int findFirstOccurrenceOfSearchStringInWorkbook(Workbook workbook, String searchString) {
         Sheet firstSheet = workbook.getSheetAt(0);
